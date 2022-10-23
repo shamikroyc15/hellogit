50d5db93bba60ed8444fae950a66bd360a0e6f19
shamikroyc15 <shamikroyc15@gmail.com>
2022-10-23 03:44:33 -0400

    added changes to [prj: hellogit + hellomiddleearth] - Dev2

--- Hellogit/src/com/shamik/HelloGit.java
+++ Hellogit/src/com/shamik/HelloGit.java
@@ -1,11 +1,40 @@
 package com.shamik;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		
 		//Adding a comment
 		System.out.println("Hello Git !!!");
+	
+		// Initialize a List with Contents
+		List<String> lines = Arrays.asList("spring", "node", "mkyong");
+		
+		// Get Filtered List
+		List<String> result = getFilterOutput(lines, "mkyong");
+		
+		// Print Filtered List
+		for(String filteredName: result)
+			System.out.println(filteredName);
+	
+	}
+	
+	private static List<String> getFilterOutput(List<String> lines, String filterOut)
+	{
+		// Declare/Initialize an Empty List
+		List<String> result = new ArrayList<String>();
+		
+		for(String aLine: lines)
+		{
+			if(!aLine.equals(filterOut))
+				result.add(aLine);
+		}
+		
+		return result;
 	}
 
 }
