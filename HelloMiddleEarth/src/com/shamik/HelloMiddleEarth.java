30cc2138c0089a40e3e030b2e934b17c16ab5ab7
shamikroyc15 <shamikroyc15@gmail.com>
2022-10-23 02:52:18 -0400

    Added 2 extra names + printout statement - Dev2

--- HelloMiddleEarth/src/com/shamik/HelloMiddleEarth.java
+++ HelloMiddleEarth/src/com/shamik/HelloMiddleEarth.java
@@ -4,11 +4,12 @@ public class HelloMiddleEarth {
 
 	public static void main(String[] args) {
 		
-		String[] names = {"Bilbo", "Fredo", "Aragorm", "Legolas", "Gandalf"};
+		String[] names = {"Bilbo", "Fredo", "Aragorm", "Legolas", "Gandalf", "Boromir", "Faramir"};
 		
 		for(int i = 0; i < names.length; i++)
 		{
 			System.out.format("Hello, %s! \n", names[i]);
+			System.out.println("How are you doing today !!");
 		}
 	}
 
